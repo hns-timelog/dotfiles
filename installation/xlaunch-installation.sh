e360cc8a691e2288a2b795286fb0259bbbd5841f
hns-timelog <81544681+hns-timelog@users.noreply.github.com>
2021-10-05 10:01:03 +0800

    Update xlaunch-installation.sh to rename username

--- installation/xlaunch-installation.sh
+++ installation/xlaunch-installation.sh
@@ -5,4 +5,4 @@ sudo apt update
 sudo apt install libgtk-3-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 -y
 
 # enable passwordless login for display
-sudo bash -c "echo 'lewis ALL = (root) NOPASSWD: /etc/init.d/dbus' >> /etc/sudoers.d/dbus"
+sudo bash -c "echo 'hns ALL = (root) NOPASSWD: /etc/init.d/dbus' >> /etc/sudoers.d/dbus"
